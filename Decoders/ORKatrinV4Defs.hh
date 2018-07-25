759626721af6591df7be6a4f303e37d119bfbc1b
kopmann <andreas.kopmann@kit.edu>
2018-07-25 17:23:03 +0000

    Added file with definitions for Katrin DAQ electronics (copy from Orca)
    git-svn-id: svn://orca.physics.unc.edu/OrcaRoot@11973 fd86f90f-2c0a-422a-b972-84106d8db31b

---
+++ Decoders/ORKatrinV4Defs.hh
@@ -0,0 +1,34 @@
+// ORKatrinV4Defs.hh
+
+// Definition talen from KatrinV4_HW_Definitions.h
+
+/*
+ enum katrinDaqMode {
+ kKatrinV4Flt_EnergyDaqMode              = 0,
+ kKatrinV4Flt_EnergyTraceDaqMode         = 1,
+ kKatrinV4Flt_Histogram_DaqMode          = 2,
+ kKatrinV4Flt_VetoEnergyDaqMode          = 3,
+ kKatrinV4Flt_VetoEnergyTraceDaqMode     = 4,
+ //kKatrinV4Flt_VetoEnergyAutoDaqMode      = 5, //for future use
+ //kKatrinV4Flt_VetoEnergyTraceSyncDaqMode    = 6, //for future use
+ //kKatrinV4Flt_EnergyTraceSyncDaqMode     = 7,
+ kKatrinV4Flt_BipolarEnergyDaqMode       = 8,
+ kKatrinV4Flt_BipolarEnergyTraceDaqMode  = 9,
+ kKatrinV4Flt_NumberOfDaqModes // do not assign a value, the compiler will do it
+ };
+ */
+
+
+#define kKatrinV4Flt_EnergyDaqMode              0
+#define kKatrinV4Flt_EnergyTraceDaqMode         1
+#define kKatrinV4Flt_Histogram_DaqMode          2
+#define kKatrinV4Flt_VetoEnergyDaqMode          3
+#define kKatrinV4Flt_VetoEnergyTraceDaqMode     4
+#define kKatrinV4Flt_VetoEnergyAutoDaqMode      5 //for future use
+#define kKatrinV4Flt_VetoEnergyTraceSyncDaqMode 6 //for future use
+#define kKatrinV4Flt_EnergyTraceSyncDaqMode     7
+#define kKatrinV4Flt_BipolarEnergyDaqMode       8
+#define kKatrinV4Flt_BipolarEnergyTraceDaqMode  9
+
+
+
